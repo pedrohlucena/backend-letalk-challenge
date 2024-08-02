6ee9187453679cbb492caf41c2c28c66ff7b5059
Pedro <pedrohlucena413@gmail.com>
2024-08-02 18:51:09 -0300

    feat: add eslint config

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express()
 app.use(json())
 
 app.use((err: Error, _: Request, res: Response, _2: NextFunction) => {
-    res.status(500).json({message: err.message})
+  res.status(500).json({ message: err.message })
 })
 
 app.listen(3000)
